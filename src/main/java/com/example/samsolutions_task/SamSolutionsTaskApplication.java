2ba6c864d01c380e6636d5946e69f57d966436f4
Viachalau <tsarenkovslava@yandex.ru>
2022-01-11 16:07:42 +0300

    init project

---
+++ src/main/java/com/example/samsolutions_task/SamSolutionsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.samsolutions_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamSolutionsTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamSolutionsTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samsolutions_task/SamSolutionsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samsolutions_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamSolutionsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
