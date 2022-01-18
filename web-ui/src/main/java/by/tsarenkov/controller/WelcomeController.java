03d4860bed2fe8244b3fc7890c8dfb0379894132
Viachalau <tsarenkovslava@yandex.ru>
2022-01-19 02:26:54 +0300

    deleted files

---
+++ web-ui/src/main/java/by/tsarenkov/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package by.tsarenkov.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping(value={"/"})
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+}
