bc403824275f9dd0f30c3dd7938f1d844b7bc308
Viachalau <tsarenkovslava@yandex.ru>
2022-01-21 01:56:41 +0300

    changed AuthorService

--- service/src/main/java/by/tsarenkov/service/AuthorService.java
+++ service/src/main/java/by/tsarenkov/service/AuthorService.java
@@ -1,4 +1,10 @@
 package by.tsarenkov.service;
 
+import by.tsarenkov.common.model.entity.Author;
+
 public interface AuthorService {
+    void saveAuthor(Author author);
+    void deleteAuthor(Author author);
+    void updateAuthor(Author author);
+    Author getAuthor(long id);
 }
--- service/src/main/java/by/tsarenkov/service/impl/AuthorServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/AuthorServiceImpl.java
@@ -1,6 +1,26 @@
 package by.tsarenkov.service.impl;
 
+import by.tsarenkov.common.model.entity.Author;
 import by.tsarenkov.service.AuthorService;
 
 public class AuthorServiceImpl implements AuthorService {
+    @Override
+    public void saveAuthor(Author author) {
+
+    }
+
+    @Override
+    public void deleteAuthor(Author author) {
+
+    }
+
+    @Override
+    public void updateAuthor(Author author) {
+
+    }
+
+    @Override
+    public Author getAuthor(long id) {
+        return null;
+    }
 }
