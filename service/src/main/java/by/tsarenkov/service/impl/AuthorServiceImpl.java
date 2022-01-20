640b4942a6adcc587253a54e7f10feef80fc2a3b
Viachalau <tsarenkovslava@yandex.ru>
2022-01-21 02:00:30 +0300

    added CRUD Author

--- service/src/main/java/by/tsarenkov/service/impl/AuthorServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/AuthorServiceImpl.java
@@ -1,26 +1,34 @@
 package by.tsarenkov.service.impl;
 
 import by.tsarenkov.common.model.entity.Author;
+import by.tsarenkov.db.repository.AuthorRepository;
 import by.tsarenkov.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorServiceImpl implements AuthorService {
+
+    @Autowired
+    private AuthorRepository repository;
+
     @Override
     public void saveAuthor(Author author) {
-
+        repository.save(author);
     }
 
     @Override
     public void deleteAuthor(Author author) {
-
+        repository.delete(author);
     }
 
     @Override
     public void updateAuthor(Author author) {
-
+        repository.saveAndFlush(author);
     }
 
     @Override
     public Author getAuthor(long id) {
-        return null;
+        return repository.getOne(id);
     }
 }
