3aeb1a8c449409dbb91db5c8edf845aa5c0bceb9
Viachalau <tsarenkovslava@yandex.ru>
2022-01-28 00:04:32 +0300

    added method existsByEmail()

--- db/src/main/java/by/tsarenkov/db/repository/UserRepository.java
+++ db/src/main/java/by/tsarenkov/db/repository/UserRepository.java
@@ -2,6 +2,9 @@ package by.tsarenkov.db.repository;
 
 import by.tsarenkov.common.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
