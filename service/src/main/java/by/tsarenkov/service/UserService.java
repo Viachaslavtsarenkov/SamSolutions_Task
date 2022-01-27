aca12cee7c26c13f77219e6d801116dc2367c242
Viachalau <tsarenkovslava@yandex.ru>
2022-01-28 00:06:13 +0300

    added UserService

--- service/src/main/java/by/tsarenkov/service/UserService.java
+++ service/src/main/java/by/tsarenkov/service/UserService.java
@@ -1,4 +1,12 @@
 package by.tsarenkov.service;
 
+import by.tsarenkov.common.model.entity.User;
+import by.tsarenkov.service.exception.MailAlreadyInUse;
+
 public interface UserService {
+   User registerUser(User user) throws MailAlreadyInUse;
+   boolean checkUserByEmail(String email);
+   void deleteUser(Long id);
+   void updateUser(User user);
+   void getAllUsers();
 }
