87bc63a4ab3dffa7b41cfac0de674b0f9df61fdc
Viachalau <tsarenkovslava@yandex.ru>
2022-01-29 13:20:05 +0300

    refactored repository

--- db/src/main/java/by/tsarenkov/db/repository/BookRepository.java
+++ db/src/main/java/by/tsarenkov/db/repository/BookRepository.java
@@ -2,6 +2,8 @@ package by.tsarenkov.db.repository;
 
 import by.tsarenkov.common.model.entity.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
