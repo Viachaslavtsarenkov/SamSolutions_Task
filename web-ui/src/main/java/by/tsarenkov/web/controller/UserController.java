07860415a7dbe9abcdf1a5b880f2db9f9b52b156
Viachalau <tsarenkovslava@yandex.ru>
2022-01-29 13:17:41 +0300

    created UserController

--- web-ui/src/main/java/by/tsarenkov/web/controller/UserController.java
+++ web-ui/src/main/java/by/tsarenkov/web/controller/UserController.java
@@ -2,34 +2,59 @@ package by.tsarenkov.web.controller;
 
 import by.tsarenkov.common.model.UserDto;
 import by.tsarenkov.common.model.entity.User;
+import by.tsarenkov.service.UserService;
+import by.tsarenkov.service.exception.MailAlreadyInUse;
+import by.tsarenkov.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/{id}")
-    public void getUser(@PathVariable Long id) {
+    private UserService userService;
 
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/{id}")
+    public void getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
     }
 
     @PatchMapping("/{id}")
     public void updateUser(@PathVariable Long id, @RequestBody User user) {
-
+        userService.updateUser(user);
     }
 
     @PostMapping
-    public void createUser(@RequestBody UserDto user) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        try {
+            userService.registerUser(user);
+        } catch (MailAlreadyInUse e) {
+            //todo
+        }
+        //todo change return value
+        return ResponseEntity.ok(user);
 
     }
 
     @GetMapping("/")
-    public void getAllUsers() {
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
-
+        userService.deleteUser(id);
     }
 
 }
