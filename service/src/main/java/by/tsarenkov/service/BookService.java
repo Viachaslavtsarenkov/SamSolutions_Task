ee39aee6ac692be3ecdcca598ef4683c889ff63b
Viachalau <tsarenkovslava@yandex.ru>
2022-02-01 12:28:32 +0300

    refactored BookService and BookController

--- service/src/main/java/by/tsarenkov/service/BookService.java
+++ service/src/main/java/by/tsarenkov/service/BookService.java
@@ -1,4 +1,14 @@
 package by.tsarenkov.service;
 
+import by.tsarenkov.common.model.entity.Author;
+import by.tsarenkov.common.model.entity.Book;
+
+import java.util.List;
+
 public interface BookService {
+    void saveBook(Book book);
+    void deleteBook(Long id);
+    void updateBook(Book book);
+    Book getBook(Long id);
+    List<Book> getAllBook();
 }
--- web-ui/src/main/java/by/tsarenkov/web/controller/BookController.java
+++ web-ui/src/main/java/by/tsarenkov/web/controller/BookController.java
@@ -1,7 +1,47 @@
 package by.tsarenkov.web.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import by.tsarenkov.common.model.entity.Book;
+import by.tsarenkov.service.BookService;
+import by.tsarenkov.service.impl.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookController {
+
+    private BookService service;
+
+    @Autowired
+    public void setService(BookServiceImpl service) {
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<Book> getBooks() {
+        List<Book> books = service.getAllBook();
+        return books;
+    }
+
+    @GetMapping("/{id}")
+    public Book getOne(@PathVariable Long id) {
+        Book book = service.getBook(id);
+        return book;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAuthor(@PathVariable Long id) {
+        service.deleteBook(id);
+    }
+
+    @PostMapping()
+    public void createBook(@RequestBody Book book) {
+        service.saveBook(book);
+    }
+
+    @PatchMapping("/{id}")
+    public void updateAuthor(@RequestBody Book book, @PathVariable Long id) {
+        service.updateBook(book);
+    }
 }
