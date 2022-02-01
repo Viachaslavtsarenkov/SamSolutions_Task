f8bb55c1ca64805e1aa8c3553ab45eb60875c678
Viachalau <tsarenkovslava@yandex.ru>
2022-02-01 12:21:52 +0300

    added CartService and CartRepository

--- service/src/main/java/by/tsarenkov/service/CartService.java
+++ service/src/main/java/by/tsarenkov/service/CartService.java
@@ -1,4 +1,13 @@
 package by.tsarenkov.service;
 
+import by.tsarenkov.common.model.entity.Author;
+import by.tsarenkov.common.model.entity.Cart;
+
+import java.util.List;
+
 public interface CartService {
+    void saveBookToCart(Long bookId, Long userId);
+    void deleteBookFromCart(Long boodId, Long userId);
+    Cart getCart(Long idUser);
+    List<Author> getAllAuthors();
 }
--- service/src/main/java/by/tsarenkov/service/impl/CartServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/CartServiceImpl.java
@@ -1,6 +1,31 @@
 package by.tsarenkov.service.impl;
 
+import by.tsarenkov.common.model.entity.Author;
+import by.tsarenkov.common.model.entity.Cart;
 import by.tsarenkov.service.CartService;
 
+import java.util.List;
+
 public class CartServiceImpl implements CartService {
+
+
+    @Override
+    public void saveBookToCart(Long bookId, Long userId) {
+
+    }
+
+    @Override
+    public void deleteBookFromCart(Long boodId, Long userId) {
+
+    }
+
+    @Override
+    public Cart getCart(Long idUser) {
+        return null;
+    }
+
+    @Override
+    public List<Author> getAllAuthors() {
+        return null;
+    }
 }
