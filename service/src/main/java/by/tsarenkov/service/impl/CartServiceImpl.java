c8441e48acf8c9c03256e6c8b5d32f9c6ea67321
Viachalau <tsarenkovslava@yandex.ru>
2022-02-01 12:30:40 +0300

    refactored services

--- service/src/main/java/by/tsarenkov/service/impl/CartServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/CartServiceImpl.java
@@ -3,9 +3,11 @@ package by.tsarenkov.service.impl;
 import by.tsarenkov.common.model.entity.Author;
 import by.tsarenkov.common.model.entity.Cart;
 import by.tsarenkov.service.CartService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CartServiceImpl implements CartService {
 
 
--- service/src/main/java/by/tsarenkov/service/impl/GenreServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/GenreServiceImpl.java
@@ -1,6 +1,8 @@
 package by.tsarenkov.service.impl;
 
 import by.tsarenkov.service.GenreService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GenreServiceImpl implements GenreService {
 }
