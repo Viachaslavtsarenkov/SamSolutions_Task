627ba7ec595239c3d7cfa369396fd97562a74f69
Viachalau <tsarenkovslava@yandex.ru>
2022-02-01 12:21:16 +0300

    added book controller

--- service/src/main/java/by/tsarenkov/service/impl/BookServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/BookServiceImpl.java
@@ -1,6 +1,44 @@
 package by.tsarenkov.service.impl;
 
+import by.tsarenkov.common.model.entity.Book;
+import by.tsarenkov.db.repository.BookRepository;
 import by.tsarenkov.service.BookService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Override
+    public Book getBook(Long id) {
+        //return bookRepository.findById(id);
+        return null;
+    }
+
+    @Override
+    public List<Book> getAllBook() {
+        return (List<Book>) bookRepository.findAll();
+    }
 }
