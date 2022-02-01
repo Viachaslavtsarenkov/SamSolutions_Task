1b9d433491f22e7572688a17888c76fe0a11bfe0
Viachalau <tsarenkovslava@yandex.ru>
2022-02-01 12:25:41 +0300

    added SaleService and SaleController

--- service/src/main/java/by/tsarenkov/service/SaleService.java
+++ service/src/main/java/by/tsarenkov/service/SaleService.java
@@ -1,4 +1,9 @@
 package by.tsarenkov.service;
 
+import by.tsarenkov.common.model.entity.Sale;
+
 public interface SaleService {
+    void saveSale(Sale sale);
+    void deleteSale(Long id);
+    void changeSale(Sale sale);
 }
--- service/src/main/java/by/tsarenkov/service/impl/SaleServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/SaleServiceImpl.java
@@ -1,6 +1,33 @@
 package by.tsarenkov.service.impl;
 
+import by.tsarenkov.common.model.entity.Sale;
+import by.tsarenkov.db.repository.SaleRepository;
 import by.tsarenkov.service.SaleService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
 public class SaleServiceImpl implements SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    @Override
+    public void saveSale(Sale sale) {
+        saleRepository.save(sale);
+    }
+
+    @Override
+    public void deleteSale(Long id) {
+        saleRepository.deleteById(id);
+    }
+
+    @Override
+    public void changeSale(Sale sale) {
+        saleRepository.save(sale);
+    }
 }
