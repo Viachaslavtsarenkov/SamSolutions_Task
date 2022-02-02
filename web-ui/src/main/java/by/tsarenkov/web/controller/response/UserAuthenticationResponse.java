03b90bd056536e168ffb8d8a6a2b8da6a0114e22
Viachalau <tsarenkovslava@yandex.ru>
2022-02-02 23:04:09 +0300

    added response classes

---
+++ web-ui/src/main/java/by/tsarenkov/web/controller/response/UserAuthenticationResponse.java
@@ -0,0 +1,12 @@
+package by.tsarenkov.web.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserAuthenticationResponse {
+    private String response;
+}
