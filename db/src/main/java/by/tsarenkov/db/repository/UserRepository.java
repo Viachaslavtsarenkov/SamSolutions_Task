86b0c0a15a272ef37b89ab60a1f37dde8271ca74
Viachalau <tsarenkovslava@yandex.ru>
2022-02-11 11:26:26 +0300

    implemented activation at UserRepository.java

--- db/src/main/java/by/tsarenkov/db/repository/UserRepository.java
+++ db/src/main/java/by/tsarenkov/db/repository/UserRepository.java
@@ -6,13 +6,28 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByEmail(String email);
     @Modifying
     @Query("update User user set user.status = :newStatus where user.id = :id")
-    void changeUserStatus(@Param(value = "id") Long id, @Param(value = "newStatus")UserStatus newStatus);
-    User findByEmail(@Param(value = "email") String email);
+    void changeUserStatus(@Param(value = "id") Long id, @Param(value = "newStatus") UserStatus newStatus);
+    @Transactional
+    @Modifying
+    @Query("update User user set user.status = :newStatus where user.email = :email and user.code = :code")
+    void activateUser(@Param(value = "email") String email,
+                      @Param(value = "newStatus") UserStatus newStatus,
+                      @Param(value = "code") String code);
+    @Transactional
+    @Modifying
+    @Query("update User user set user.password = :password where user.email = :email")
+    void resetUserPassword(@Param(value = "password") String password,
+                           @Param(value = "email") String email);
+
+    User getByEmail(String email);
 }
