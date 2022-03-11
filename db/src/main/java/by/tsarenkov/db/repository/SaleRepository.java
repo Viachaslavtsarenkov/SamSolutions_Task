d3f86e1b0ec3eadace902c67673ed8d495ad3e0b
Viachalau <tsarenkovslava@yandex.ru>
2022-03-11 06:44:52 +0300

    implemented Sales pagination

--- db/src/main/java/by/tsarenkov/db/repository/SaleRepository.java
+++ db/src/main/java/by/tsarenkov/db/repository/SaleRepository.java
@@ -1,6 +1,8 @@
 package by.tsarenkov.db.repository;
 
 import by.tsarenkov.common.model.entity.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.sql.Date;
 
 @Repository
 public interface SaleRepository extends CrudRepository<Sale, Long> {
-
+    Page<Sale> findAll(Pageable pageable);
 }
--- service/src/main/java/by/tsarenkov/service/SaleService.java
+++ service/src/main/java/by/tsarenkov/service/SaleService.java
@@ -1,9 +1,15 @@
 package by.tsarenkov.service;
 
 import by.tsarenkov.common.model.entity.Sale;
+import by.tsarenkov.common.model.payload.SalePageResponse;
+import by.tsarenkov.service.exception.SaleNotFoundException;
+
+import java.util.List;
 
 public interface SaleService {
-    void saveSale(Sale sale);
+    Sale saveSale(Sale sale);
     void deleteSale(Long id);
-    void changeSale(Sale sale);
+    Sale getSaleById(Long id) throws SaleNotFoundException;
+    void updateSale(Sale sale);
+    SalePageResponse getSales(int page);
 }
