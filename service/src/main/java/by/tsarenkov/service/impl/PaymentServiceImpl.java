d55685fbca1942361093f7f21c4d90cfbaa1bbe2
Viachalau <tsarenkovslava@yandex.ru>
2022-03-11 06:44:17 +0300

    started to work with Payment

--- service/src/main/java/by/tsarenkov/service/impl/PaymentServiceImpl.java
+++ service/src/main/java/by/tsarenkov/service/impl/PaymentServiceImpl.java
@@ -1,8 +1,48 @@
 package by.tsarenkov.service.impl;
 
+import by.tsarenkov.common.model.entity.Payment;
+import by.tsarenkov.common.model.payload.PaymentPageResponse;
+import by.tsarenkov.db.repository.PaymentRepository;
 import by.tsarenkov.service.PaymentService;
+import by.tsarenkov.service.exception.PaymentNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @Override
+    public Payment savePayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public void deletePayment(Long id) {
+        paymentRepository.deleteById(id);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) throws PaymentNotFoundException {
+        return Optional.of(paymentRepository.findById(id))
+                .get().orElseThrow(PaymentNotFoundException::new);
+    }
+
+    @Override
+    public void updatePayment(Payment payment) {
+        paymentRepository.save(payment);
+    }
+
+    @Override
+    public PaymentPageResponse getPaymentPage(int page) {
+        Page<Payment> paymentPage = paymentRepository
+                .findAll(PageRequest.of(page, 10));
+        return new PaymentPageResponse(paymentPage.getContent(), paymentPage.getTotalPages());
+    }
 }
